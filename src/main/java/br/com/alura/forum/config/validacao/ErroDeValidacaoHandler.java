58cd272dfa73f1643565a3bdf1fb83bda401941b
Rafael Enes <rafaeleneswork@gmail.com>
2021-03-17 21:16:34 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.config.validacao;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErroDeValidacaoHandler {
+	
+	public void handle() {
+		
+		
+	}
+
+}
---
+++ src/main/java/br/com/alura/forum/javax/validation/constraints.java
@@ -0,0 +1,5 @@
+package br.com.alura.forum.javax.validation;
+
+public class constraints {
+
+}
